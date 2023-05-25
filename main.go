ddf66f10bd1247a1ca3499529f7a365345013e39
Denny <dennygod1220@gmail.com>
2023-05-25 11:08:48 +0800

    Feat : 調整 Main 初始化 DB 方式

--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"denny/gin_gorm_practise/app/db"
 	"denny/gin_gorm_practise/app/router"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func main() {
-	d := &db.Sqlite{
-		DBName: "test.DB",
+	var d db.IDb
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 
-	DB = db.InitDb(d)
+	if os.Getenv("APP_ENV") == "testing" {
+		d = &db.Sqlite{}
+	} else {
+		d = &db.Mysql{}
+	}
+
+	DB = db.InitDb(d.NewConnInfo())
 
 	router := router.InitRouter()
 
